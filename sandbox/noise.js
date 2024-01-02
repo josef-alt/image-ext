cd11bd575509b29102a7a6672b6395218d82e870
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-01-01 18:40:08 -0600

    resolved issue with noise extraction not finishing on wide/tall images

--- sandbox/noise.js
+++ sandbox/noise.js
@@ -14,48 +14,44 @@ function noise(base64) {
 			canvas.width = width;
 			canvas.height = height;
 			context.drawImage(image, 0, 0);
-			
 			const pixels = context.getImageData(0, 0, width, height).data;
 			
-			// compute one dimensional index from cartesian point
-			function pos(x, y) {
-				return (y * width + x) * 4;
+			const pos = (y, x) => {
+				return y * (width * 4) + x * 4;
 			}
 			
-			// binary threshold for the median
-			function f(l, c, r) {
-				const max = Math.max(l, c, r);
-				const min = Math.min(l, c, r);
-				const med = l ^ c ^ r ^ min ^ max;
-				if(med == c)
+			const median = arr => {
+				const mid = 4;
+				const nums = [...arr].sort((a, b) => a - b);
+				return nums[mid];
+			};
+			
+			const f = arr => {
+				const mid = arr[4];
+				if(median(arr) == mid)
 					return 0;
 				return 255;
-			}
+			};
 			
 			// store the extracted noise
 			var buffer = new Uint8ClampedArray(width * height * 4);
-			
-			// an attempt at a seperated median filter
 			for(var row = 1; row < height - 1; ++row) {
-				for(var col = 1; col < width - 1; ++col) {
-					let left  = pixels[pos(row, col - 1)];
-					let curr  = pixels[pos(row, col)];
-					let right = pixels[pos(row, col + 1)];
-					let val = f(left, curr, right);
-					for(var cnl = 0; cnl < 4; ++cnl) {
+				for(var col = 1; col < width - 1; ++col) {	
+					let val = f([
+						pixels[pos(row - 1, col - 1)], 
+						pixels[pos(row - 1, col)],
+						pixels[pos(row - 1, col + 1)],
+						pixels[pos(row, col - 1)],
+						pixels[pos(row, col)],
+						pixels[pos(row, col + 1)],
+						pixels[pos(row + 1, col - 1)],
+						pixels[pos(row + 1, col)],
+						pixels[pos(row + 1, col + 1)]]);
+						
+					for(var cnl = 0; cnl < 3; ++cnl) {
 						buffer[pos(row, col) + cnl] = val;
 					}
-				}
-			}
-			for(var row = 1; row < height - 1; ++ row) {
-				for(var col = 1; col < width - 1; ++col) {
-					let above = pixels[pos(row - 1, col)];
-					let curr  = pixels[pos(row, col)];
-					let below = pixels[pos(row + 1, col)];
-					let val = f(above, curr, below);
-					for(var cnl = 0; cnl < 4; ++cnl) {
-						buffer[pos(row, col) + cnl] = val;
-					}
+					buffer[pos(row, col) + 3] = 255;
 				}
 			}
 			
